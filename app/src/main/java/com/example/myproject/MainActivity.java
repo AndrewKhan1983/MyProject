e3cbe46ad254db1dcbb11245b502788a67a6abc0
andre <andre-khan@yandex.ru>
2021-09-23 22:46:54 +0300

    ДЗ

--- app/src/main/java/com/example/myproject/MainActivity.java
+++ app/src/main/java/com/example/myproject/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> products = new ArrayList<String>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
